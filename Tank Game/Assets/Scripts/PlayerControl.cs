8f1c753dd89643e874f01e89ebdc270b6d880ac7
sokama <kovacs.beno@freemail.hu>
2016-10-28 19:20:17 +0200

    Player movement
    Player irányítása nyilakkal/wasd-vel. a tank ágyúja egyelőre nem forog.

---
+++ Tank Game/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public float MovementSpeed;
+    public float RotationSpeed;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * RotationSpeed * Input.GetAxis("Horizontal") * Input.GetAxis("Vertical") * Time.deltaTime);
+        rb.velocity = transform.forward * MovementSpeed * Input.GetAxis("Vertical");
+    }
+}

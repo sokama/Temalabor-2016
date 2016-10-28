c09770c65ade251aed22ac69f5043bb6faf46b3d
sokama <kovacs.beno@freemail.hu>
2016-10-28 21:35:55 +0200

    Tower rotation
    A játékos tankjának ágyúját az egérrel lehet forgatni.

--- Tank Game/Assets/Scripts/PlayerControl.cs
+++ Tank Game/Assets/Scripts/PlayerControl.cs
@@ -9,14 +9,50 @@ public class PlayerControl : MonoBehaviour
 
     Rigidbody rb;
 
-    void Start()
+    int raycastFloorMask;
+    float raycastLength = 100f;
+
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        raycastFloorMask = LayerMask.GetMask("RaycastFloor");
     }
 
     void Update()
     {
-        transform.Rotate(Vector3.up * RotationSpeed * Input.GetAxis("Horizontal") * Input.GetAxis("Vertical") * Time.deltaTime);
-        rb.velocity = transform.forward * MovementSpeed * Input.GetAxis("Vertical");
+        MovePlayer();
+        RotateTower();
+    }
+
+    private void MovePlayer()
+    {
+        transform.Rotate(Vector3.up * RotationSpeed * Input.GetAxisRaw("Horizontal") * Input.GetAxisRaw("Vertical") * Time.deltaTime);
+        rb.velocity = transform.forward * MovementSpeed * Input.GetAxisRaw("Vertical");
+    }
+
+    private void RotateTower()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit raycastFloorHit;
+        if(Physics.Raycast(camRay, out raycastFloorHit, raycastLength, raycastFloorMask))
+        {
+            Vector3 fromPlayerToMouse = raycastFloorHit.point - transform.position;
+            fromPlayerToMouse.y = 0f;
+
+            Quaternion rotation = Quaternion.LookRotation(fromPlayerToMouse);
+
+            GameObject tower = transform.FindChild("Graphics").FindChild("Tower").gameObject;
+
+            if (tower == null) Debug.Log("tower is null");
+
+            //Rigidbody rbTower = tower.GetComponent<Rigidbody>();
+
+            //if (rbTower == null) Debug.Log("rbTower is null");
+
+            //rbTower.MoveRotation(rotation);
+
+            tower.transform.rotation = rotation;
+            tower.transform.Rotate(new Vector3(-90f, 0f, 0f)); //ez azért kell, mert rossz a tank 3D modellje, és alapból -90 fokkal el kell forgatni az X tengely körül...
+        }
     }
 }

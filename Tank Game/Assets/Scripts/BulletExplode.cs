a5cf4b6bc76d80c1ffb26b0d2a48f26c8b84285b
sokama <kovacs.beno@freemail.hu>
2016-10-29 17:08:52 +0200

    Shooting + Health
    A player balkattintással tud lőni (egyelőre akármennyit, akármilyen
    sűrűn). A playernek és az enemynek lett élete (de bármire rá lehet
    ültetni a Health.cs scriptet, jól fog működni). Ha a lövedék valami
    olyat talál el, aminek van élete, csökkenti azt a saját damage-ével.
    Akinek elfogy az élete az meghal.

---
+++ Tank Game/Assets/Scripts/BulletExplode.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BulletExplode : MonoBehaviour {
+
+    public float Damage = 10f;
+    public string PlayerTag = "Player";
+    public string EnemyTag = "Enemy";
+
+    void OnTriggerEnter(Collider col)
+    {
+        //if (col.tag == PlayerTag || col.tag == EnemyTag)
+        //{
+        //    Hit(col.gameObject);
+        //}
+
+        if(col.GetComponent<Health>() != null)
+        {
+            col.GetComponent<Health>().DecreaseHealth(Damage);
+        }
+
+        Explode();
+    }
+
+    //private void Hit(GameObject hitTarget)
+    //{
+    //    Debug.Log("HIT");
+    //}
+
+    private void Explode()
+    {
+        Destroy(gameObject);
+        return;
+    }
+}
---
+++ Tank Game/Assets/Scripts/Health.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+    public float StartHealth = 100;
+    private float currentHealth;
+
+    void Start()
+    {
+        currentHealth = StartHealth;
+    }
+
+    public void DecreaseHealth(float damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("Health: " + currentHealth);
+        if(currentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        return;
+    }
+}

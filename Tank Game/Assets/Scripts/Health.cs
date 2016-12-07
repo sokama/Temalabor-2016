db47226b08ba482eec59149484d0a3347aab5ce6
sokama <kovacs.beno@freemail.hu>
2016-12-07 01:10:50 +0100

    Health bar, health regeneration
    A játékos és az ellenség feje fölött lebeg egy-egy életcsík, mindkettő
    folyamatosan a kamera felé néz. Az élet adott időközönként adott
    mennyiséggel növekszik.

--- Tank Game/Assets/Scripts/Health.cs
+++ Tank Game/Assets/Scripts/Health.cs
@@ -1,35 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Health : MonoBehaviour {
 
-    public GameObject ExplosionParticles;
-    public float StartHealth = 100;
+    public GameObject explosionParticles;
+    public GameObject healthBar;
+
+    public float startHealth = 100f;
+    public float healthRegenerationUnit = 1f;
+    public float healthRegenerationTime = 1f;
+
     private float currentHealth;
 
     void Start()
     {
-        currentHealth = StartHealth;
+        currentHealth = startHealth;
+        InvokeRepeating("HealthRegeneration", healthRegenerationTime, healthRegenerationTime);
     }
 
     public void DecreaseHealth(float damage)
     {
         currentHealth -= damage;
-        if(this.tag == "Enemy")
+
+        if (this.tag == "Enemy")
             HidingFromPlayer.notifiyHealth((int)currentHealth);
+
         Debug.Log("Health: " + currentHealth);
-        if(currentHealth <= 0f)
+
+        if (currentHealth <= 0f)
         {
             Die();
         }
+
+        UpdateHealthBar();
     }
 
     private void Die()
     {
-        GameObject explosion = (GameObject)Instantiate(ExplosionParticles, transform.position, transform.rotation);
+        GameObject explosion = (GameObject)Instantiate(explosionParticles, transform.position, transform.rotation);
         Destroy(explosion, 2f);
 
         Destroy(gameObject);
         return;
+    }
+
+    private void HealthRegeneration()
+    {
+        currentHealth += healthRegenerationUnit;
+        if (currentHealth > startHealth)
+            currentHealth = startHealth;
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        if (healthBar != null)
+            healthBar.GetComponent<HealthBarManager>().UpdateHealthBar(currentHealth, startHealth);
     }
 }
---
+++ Tank Game/Assets/Scripts/HealthBarManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarManager : MonoBehaviour {
+
+    public Transform mainCamera;
+    public GameObject bar;
+
+    public void Update()
+    {
+        transform.rotation = mainCamera.rotation;
+    }
+
+    public void UpdateHealthBar(float currentHealth, float maxHealth)
+    {
+        bar.transform.localScale = new Vector3(currentHealth / maxHealth, 1f, 1f);
+    }
+}
